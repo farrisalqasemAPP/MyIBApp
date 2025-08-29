a6455689a3f2ccf294a782e57b1f8bb26e0e20c5
farrisalqasemAPP <farrisalqasem@gmail.com>
2025-08-29 13:51:52 +0300

    feat: add settings and light mode

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,17 +1,26 @@
 /**
- * Color palette for the app using only dark mode.
+ * Color palette for the app supporting light and dark modes.
  */
 
-const tintColorDark = '#9c5dfc';
+const tintColor = '#9c5dfc';
 
 export const Colors = {
+  light: {
+    text: '#000000',
+    background: '#ffffff',
+    card: '#f2f2f2',
+    tint: tintColor,
+    icon: '#555555',
+    tabIconDefault: '#555555',
+    tabIconSelected: tintColor,
+  },
   dark: {
     text: '#ffffff',
     background: '#121212',
     card: '#1e1e1e',
-    tint: tintColorDark,
+    tint: tintColor,
     icon: '#9a9a9a',
     tabIconDefault: '#9a9a9a',
-    tabIconSelected: tintColorDark,
+    tabIconSelected: tintColor,
   },
-};
+} as const;

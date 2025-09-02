11f7fba80dce4abf607272cbf7a0247aab1b5ee1
farrisalqasemAPP <farrisalqasem@gmail.com>
2025-09-02 20:34:05 +0300

    Apply gradient background and light theme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,25 +2,26 @@
  * Color palette for the app supporting light and dark modes.
  */
 
-const tintColor = '#9c5dfc';
+const darkTintColor = '#9c5dfc';
+const lightTintColor = '#9370db';
 
 export const Colors = {
   light: {
     text: '#000000',
-    background: '#ffffff',
-    card: '#f2f2f2',
-    tint: tintColor,
+    background: '#add8e6',
+    card: '#d0e1ff',
+    tint: lightTintColor,
     icon: '#555555',
     tabIconDefault: '#555555',
-    tabIconSelected: tintColor,
+    tabIconSelected: lightTintColor,
   },
   dark: {
     text: '#ffffff',
     background: '#121212',
     card: '#1e1e1e',
-    tint: tintColor,
+    tint: darkTintColor,
     icon: '#9a9a9a',
     tabIconDefault: '#9a9a9a',
-    tabIconSelected: tintColor,
+    tabIconSelected: darkTintColor,
   },
 } as const;
